0d7f1108e0f8e559f5b91007ffc0650151c8ab08
m4w4q7 <m4w4q7@gmail.com>
2015-03-02 20:48:36 +0000

    server now uses "add" module and removed mocha from devDependencies

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require("express");
 var app = express();
+var add = require("./add.js");
 
 app.get('/add', function(req, res) {
     
     if (isFinite(req.query.a) && isFinite(req.query.b)) {
-        var a = parseInt(req.query.a);
-        var b = parseInt(req.query.b);
-        res.send((a + b).toString());
+        var a = parseInt(req.query.a, 10);
+        var b = parseInt(req.query.b, 10);
+        res.send(add(a, b).toString());
     } else {
         res.send('NaN');
     }
-    console.log(req.query);
+    console.log(add);
 });
 
 app.listen(process.env.PORT, function() {
